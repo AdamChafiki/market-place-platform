d8250eef99f6cee5d3720ecd63115be81f8648b9
Adam Chafiki <adamchafiki3@gmail.com>
2025-07-19 14:47:24 +0100

    init

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+import errorHandler from "./middlewares/errorHandler";
+import config from "./config/config";
+
+const app = express();
+
+app
+  .use(helmet())
+  .use(morgan("dev"))
+  .use(compression())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.use(errorHandler);
+
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}`);
+});
